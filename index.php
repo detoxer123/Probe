0c4cf6954d06d30fe5f68a2ca1746f0e163bb08c
detoxer123 <detoxer@citromail.hu>
2015-08-14 12:36:52 +0200

    Erase lines
    Create a commit #1

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
 /**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
+
  * See COPYRIGHT.txt and LICENSE.txt.
  */
 
